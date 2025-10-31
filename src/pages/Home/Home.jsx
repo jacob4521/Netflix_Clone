7b5279b4402b79012ac999ebedfd895227d453cc
Zakib <zakib6354@gmail.com>
2025-10-31 17:59:53 +0530

    Style play and more info buttons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,8 +17,8 @@ const Home = () => {
           <img src={hero_title} alt="" className='caption-img' />
           <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
           <div className="hero-btns">
-            <button><img src={play_icon} alt="" />Play</button>
-            <button><img src={info_icon} alt="" />More Info</button>
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
           </div>
         </div>
       </div>
