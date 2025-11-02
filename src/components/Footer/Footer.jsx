4910b223dd8c129c1507ca0057a22b3f05d16b73
Zakib <zakib6354@gmail.com>
2025-11-02 16:36:52 +0530

    Added the footer and installing the react-router-dom

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,26 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className="footer-icons">
-
+        <img src={facebook_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={youtube_icon} alt="" />
       </div>
+      <ul>
+          <li>Audio Description</li>
+          <li>Help Center</li>
+          <li>Gift Cards</li>
+          <li>Media Center</li>
+          <li>Investor Relations</li>
+          <li>Jobs</li>
+          <li>Terms of Use</li>
+          <li>Privacy</li>
+          <li>Legal Notices</li>
+          <li>Cookie Preferences</li>
+          <li>Corporate Information</li>
+          <li>Contact Us</li>
+      </ul>
+      <div className="copyright-text">@1997-2023 Netflix Inc</div>
     </div>
   )
 }
