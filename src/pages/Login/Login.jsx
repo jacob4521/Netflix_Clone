7edd9b0ccd4dae1c9a9084b0a64ca951a5d5dc7f
Zakib <zakib6354@gmail.com>
2025-11-02 20:15:16 +0530

    Create the login page with sign in and up forms.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
+  const [signState, setSignState] = useState("Sign In");
+
+  function handleSignState() {
+    signState === "Sign In" ? setSignState("Sign Up") : setSignState("Sign In")
+  }
+
   return (
     <div className='login'>
-      
+      <img src={logo} alt="" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" && <input type="text" placeholder='Your name' />}
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Sign In" ?
+            <p>New to Netflix? <span onClick={handleSignState}>Sign Up Now</span></p>
+            : <p>Already have account? <span onClick={handleSignState}>Sign Up Now</span></p>
+          }
+        </div>
+      </div>
     </div>
   )
 }
