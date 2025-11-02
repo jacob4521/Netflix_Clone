7b9d893952ae940dce4e6303fa3aa2e3016b110e
Zakib <zakib6354@gmail.com>
2025-11-02 21:12:54 +0530

    Fetch the cards from the api for different categories.

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,19 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
 
 
 
-const TitleCards = ({title}) => {
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDg5N2JmOGRmY2EwYjg4MjQzNDQzMzdlNDRlNmNlNSIsIm5iZiI6MTc2MjA5NTM4NS45MDIwMDAyLCJzdWIiOiI2OTA3NzExOWVlNDlkM2M1YzhmOTJmOWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.P1QFiT8LHrMkwOiDaOmal1wCNqp9RmFLEUeFQgnscGI'
+    }
+  };
 
   const handleWheel = (event) => {
     console.log(event)
@@ -14,17 +22,22 @@ const TitleCards = ({title}) => {
   }
 
   useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category? category:"now_playing"}?language=en-US&page=1`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results))
+      .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel', handleWheel)
   }, [])
 
   return (
     <div className='title-cards'>
-      <h2>{title? title:"Popular on Netflix"}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
+            <img src={`https://image.tmdb.org/t/p/original` + card.poster_path} alt="" />
+            <p>{card.original_title}</p>
           </div>
         })}
       </div>
